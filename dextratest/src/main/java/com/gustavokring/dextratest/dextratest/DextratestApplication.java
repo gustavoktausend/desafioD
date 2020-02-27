c13e9b16930fb252811952f395140777a8f6f4c7
Gustavo Kring Tausendfreund <gustavoktausend@gmail.com>
2020-02-27 01:15:59 -0300

    first commit

---
+++ dextratest/src/main/java/com/gustavokring/dextratest/dextratest/DextratestApplication.java
@@ -0,0 +1,13 @@
+package com.gustavokring.dextratest.dextratest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DextratestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DextratestApplication.class, args);
+	}
+
+}
---
+++ dextratest/src/test/java/com/gustavokring/dextratest/dextratest/DextratestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavokring.dextratest.dextratest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DextratestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
